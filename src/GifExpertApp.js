c6a685016eae0a037bac66cb1ab4ef3192d0b382
Juan Ignacio Roldan <juanignacioroldan01@gmail.com>
2020-12-01 20:40:35 -0300

    GifExpertApp actualizado

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -8,7 +8,7 @@ export const GifExpertApp = () => {
     
     return (
         <>
-            <h2>GifExpertApp</h2>
+            <h2>Buscador de GIFS </h2>
             <AddCategory setCategories={setCategories} />
             <hr />
 
